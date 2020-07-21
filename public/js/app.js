b63ed3e1c48e2f087de27efcf1e42b36e23f717d
Majey <kevinerick78@yahoo.com>
2020-07-21 22:16:13 +0300

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client side javascript file loaded");
-
 const weatherForm = document.querySelector("form")
 const searchElement = document.querySelector("input")
 const messageOne = document.querySelector("#message-1")
